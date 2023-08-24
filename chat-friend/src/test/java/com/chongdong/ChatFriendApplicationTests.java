50e580e16e41d9f480c81d18c76b3d8bccc8342d
ASUS <152153431@qq.com>
2023-08-24 10:42:36 +0800

    初始化

---
+++ chat-friend/src/test/java/com/chongdong/ChatFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chongdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
